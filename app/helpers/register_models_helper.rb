7f03abf8279f56ec8bf330239dac20956e5de661
txemagon / imasen <txema.gonz@gmail.com>
2015-09-11 00:09:49 +0200

    added versions to model registers at index.html

--- app/helpers/register_models_helper.rb
+++ app/helpers/register_models_helper.rb
@@ -1,6 +1,7 @@
 module RegisterModelsHelper
 
-	def link_to_first_version (version)
+	def link_to_first_version (model)
+		  version = model.versions.first
 		  first_version = YAML.load(version.next.object)
 		  name = first_version['name'] + " v0"
 	      filename = first_version['document_file_name']
