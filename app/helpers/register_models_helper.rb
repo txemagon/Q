3068baa714906d2d74df83670add829d93d5c763
txemagon / imasen <txema.gonz@gmail.com>
2015-09-10 23:13:48 +0200

    Register Model showing older versions

--- app/helpers/register_models_helper.rb
+++ app/helpers/register_models_helper.rb
@@ -1,2 +1,11 @@
 module RegisterModelsHelper
+
+	def link_to_first_version (version)
+		  first_version = YAML.load(version.next.object)
+		  name = first_version['name'] + " v0"
+	      filename = first_version['document_file_name']
+	      filename = filename.split('.')
+	      filename = filename.join('_v0.')
+	      link_to name, "#{RegisterModel::DOCUMENT_PATH}/#{filename}"
+	end
 end
--- app/models/register_model.rb
+++ app/models/register_model.rb
@@ -3,23 +3,25 @@ class RegisterModel < ActiveRecord::Base
   has_attached_file :document, preserve_files: "true",
 			:keep_old_files => true,
 			:url => "/system/:class/:attachment/:basename_v:version.:extension",
-    		:path => ":rails_root/public/system/:class/:attachment/:basename_v:version.:extension"
+    	:path => ":rails_root/public/system/:class/:attachment/:basename_v:version.:extension"
 
-  has_paper_trail :only => [:document]
+  DOCUMENT_PATH = "/system/register_models/documents"
+
+  has_paper_trail :ignore => [:name, :explanation]
 
   validates :name, presence: true, uniqueness: true
-  
-  validates_attachment :document, presence: true, 
+
+  validates_attachment :document, presence: true,
                                   content_type: {content_type: [
-                                  	    "application/msword", 
+                                  	    "application/msword",
                                   	    "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
                                   	    ]}
-                                  
+
   validates_attachment_file_name :document, matches: [/docx?\Z/]
-  
+
 
   Paperclip.interpolates :version do |attachment, style|
-  	attachment.instance.versions.last.id.to_s
+  	attachment.instance.versions.last.index.to_s
   end
 
 end
