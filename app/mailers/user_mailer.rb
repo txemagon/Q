f98c1f5b746664da4376f2158ef3045bb25d15dc
txemagon / imasen <txema.gonz@gmail.com>
2015-09-15 00:41:22 +0200

    Welcome email fully functional

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,14 @@
 class UserMailer < ApplicationMailer
+	default from: User.first.email
+
+	def welcome_user_email(user, pass)
+		@user = user
+		@pass = pass
+
+        attachments.inline['Q.png'] = File.read(Rails.root.join("app/assets/images", "Q120.png"))
+        attachments.inline['vertical_cat_claim.png'] = File.read(Rails.root.join("app/assets/images", "vertical_cat_claim256.png"))
+
+		mail(to: @user.email,
+			subject: "Welcome to Q")
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,19 +8,19 @@ class User < ActiveRecord::Base
 
   validates :last_name, :uniqueness => {:scope => :first_name}
 
-  after_create :send_welcome_email
-
   def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
     data = access_token.info
     user = User.where(:email => data["email"]).first
 
     # Uncomment the section below if you want users to be created if they don't exist
     unless user
+        token = Devise.friendly_token[0,20]
         user = User.create(first_name: data["first_name"],
            last_name: data["last_name"],
            email: data["email"],
-           password: Devise.friendly_token[0,20]
+           password: token
         )
+        UserMailer.welcome_user_email(user, token).deliver_later
     end
     user
   end
@@ -31,12 +31,7 @@ class User < ActiveRecord::Base
     self.first_name = omniauth['first_name']
     self.last_name  = omniauth['last_name']
     self.email      = omniauth['email']
-    self.password   = Devise.friendly_token.first(8)
-    self.password_confirmation   = Devise.friendly_token.first(8)
   end
 
-  def self.send_welcome_email(attributes={})
-    
-  end
 
 end
